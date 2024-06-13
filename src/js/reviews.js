cef39b51be71b0e180c98553fda75ae645374954
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-06-13 17:57:53 +0300

    slider-Js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -21,17 +21,17 @@ const swiper = new Swiper('.reviews-swiper', {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 20
+      spaceBetween: 
     },
     // when window width is >= 480px
     480: {
-      slidesPerView: 2,
-      spaceBetween: 30
+      slidesPerView: 1,
+      spaceBetween: 
     },
     // when window width is >= 640px
     640: {
       slidesPerView: 2,
-      spaceBetween: 40
+      spaceBetween: 32
     }
   }
 });
