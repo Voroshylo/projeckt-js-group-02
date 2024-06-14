f8e02ba5f73128ccaa947e4522a6f9dde00c018e
Ihorsoft <0639492002s@gmail.com>
2024-06-14 18:04:56 +0300

    version Header with out style css

--- src/js/header.js
+++ src/js/header.js
@@ -1,24 +1,30 @@
 
+
+console.log("start js");
+
+const storageTheme = "datThema";
 const dataThema = {
 colorThema: "",
 };
 
 const nowThema = document.querySelector("body");
-const data = JSON.parse(localStorage.getItem("typeThema"));
+const data = JSON.parse(localStorage.getItem(storageTheme));
+
+console.log("first get", data || false);
 
 if (!data) {
     console.log("localstorage is clear");
     dataThema.colorThema = "light";
      nowThema.classList.add('lightthema');
-    localStorage.setItem('typeThema', JSON.stringify(dataThema));
+    localStorage.setItem(storageTheme, JSON.stringify(dataThema));
    // return;
 }
 
-const switchThema = document.querySelector(".switchthema");
+const switchThema = document.querySelector("#sitetheme");
 switchThema.addEventListener("click", changeThema);
 
 export function changeThema(element) {
-    const data = JSON.parse(localStorage.getItem("typeThema"));
+    const data = JSON.parse(localStorage.getItem(storageTheme));
     console.log("localStore Thema:", data.colorThema);
     if (data.colorThema == 'dark') {
         
@@ -26,7 +32,7 @@ export function changeThema(element) {
             nowThema.classList.remove("darkthema");
        }
         dataThema.colorThema = "light";
-        localStorage.setItem('typeThema', JSON.stringify(dataThema));
+        localStorage.setItem(storageTheme, JSON.stringify(dataThema));
         nowThema.classList.add('lightthema');
         return;      
     }
@@ -35,8 +41,27 @@ export function changeThema(element) {
             nowThema.classList.remove("lightthema");
        }
           dataThema.colorThema = "dark";
-        localStorage.setItem('typeThema', JSON.stringify(dataThema));
+        localStorage.setItem(storageTheme, JSON.stringify(dataThema));
         nowThema.classList.add('darkthema');
         return;   
     }
 }
+
+//  function burger menu mob menu
+
+(() => {
+  const menuBtnRef = document.querySelector("[date-menu-button]");
+  const mobileMenuRef = document.querySelector("[date-menu]");
+ // const searchRef = document.querySelector(".form-search");
+  menuBtnRef.addEventListener("click", () => {
+      const expanded =
+          menuBtnRef.getAttribute("aria-expanded") === 'true' || false;
+      
+      menuBtnRef.classList.toggle("is-open");
+      menuBtnRef.setAttribute("aria-expanded", !expanded);
+      mobileMenuRef.classList.toggle("is-open");
+     // searchRef.classList.toggle("is-open");
+      document.body.classList.toggle("is-open");
+
+  });
+})();
